515b6f137000ba29978d6976613cd3bf46d8dcb8
kenia.picos <kenia.picos@cetys.mx>
2018-09-22 12:31:24 -0700

    Test commit.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
-//package edu.ucsd.cs;
+package edu.ucsd.cs;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(args[0]); // WGG - added line
     }
 
 }
